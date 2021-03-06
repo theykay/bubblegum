f367d9651d7f23066956b1312bdf70e688814676
TheyKay <murraykyleb@gmail.com>
2021-03-05 23:34:44 -0700

    removed unecessary react files, made note of colors in app.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +16,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1 id="one">h1</h1>
+      <h1 id="two">h2</h1>
+      <h1 id="three">h3</h1>
+      <h1 id="four">h4</h1>
+      <h1 id="five">h5</h1>
     </div>
   );
 }
