43844988a94b23de299961eb6c7dbda35ec36181
TheyKay <murraykyleb@gmail.com>
2021-03-15 11:13:49 -0600

    added react-router-dom as dependency, created router/switch/routes in app.js to return individual pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './App.css';
-import AboutMe from './Pages/AboutMe';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import AboutMe from "./Pages/AboutMe";
+import Handmade from "./Pages/Handmade";
+import WebDesign from "./Pages/WebDesign";
 
 const App = () => {
   return (
     <>
-      <AboutMe />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={AboutMe} />
+          <Route exact path="/web" component={WebDesign} />
+          <Route exact path="/handmade" component={Handmade} />
+        </Switch>
+      </Router>
     </>
-  )
+  );
 };
 
 export default App;
