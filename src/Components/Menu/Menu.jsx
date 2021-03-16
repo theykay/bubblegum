786c305bd40e2642d39c5828e41c9d080b5d2e85
TheyKay <murraykyleb@gmail.com>
2021-03-16 14:13:16 -0600

    added react fortawesome

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './menu.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Menu = () => {
   return (
     <>
       <nav>
-          <button class="dd-trigger">menu</button>
+          <button class="dd-trigger"><FontAwesomeIcon icon={faBars} /></button>
           <ul class="dd-menu">
             <li class="ddItem dd1"><a class="ddLink" href="/">about</a></li>
             <li class="ddItem dd2"><a class="ddLink" href="/digital">apps</a></li>
