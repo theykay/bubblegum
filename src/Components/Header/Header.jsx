dc65462f1d78eb6ac06f84db1635dfe8820b855c
TheyKay <murraykyleb@gmail.com>
2021-03-17 17:45:56 -0600

    styling header and placeholder logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,22 +6,12 @@ const Header = () => {
   return (
     <>
       <header id="pageHead">
-        {/* logo */}
-        <div>logo</div>
+        {/* placeholder for logo */}
+        <section id="meTitle">
+          <div id="logo"></div>
+          Kay
+        </section>
         <Menu />
-        {/* <div class="dropdown">
-          <ul class="dropdown-menu dropdown-menu--animated">
-            <li class="dropdown-item-1">
-              <a href="/">About Me</a>
-            </li>
-            <li class="dropdown-item-2">
-              <a href="/digital">Web Projects</a>
-            </li>
-            <li class="dropdown-item-3">
-              <a href="/handmade">Handmade Projects</a>
-            </li>
-          </ul>
-        </div> */}
       </header>
     </>
   )
