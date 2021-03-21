266f09990f059dc21620392276645f60fd68fe34
TheyKay <murraykyleb@gmail.com>
2021-03-21 14:20:08 -0600

    changes to get app to run in heroku

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public')
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT: ${PORT}`);
+});
