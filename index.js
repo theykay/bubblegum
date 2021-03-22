69a7c95de7a18aadea9c6c83958b311c32dd0f5d
TheyKay <murraykyleb@gmail.com>
2021-03-22 16:27:23 -0600

    added _redirects file for netlify to handle react router cases

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public')
+const publicPath = path.join(__dirname, 'public')
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
