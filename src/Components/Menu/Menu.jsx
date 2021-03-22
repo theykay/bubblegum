846888dc2dd286d967431b2d41e3c0d44e1f2d06
TheyKay <murraykyleb@gmail.com>
2021-03-22 16:32:58 -0600

    replaced a tags in menu with react-router-dom Link components to play nice with netlify

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './menu.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'
@@ -10,13 +11,16 @@ const Menu = () => {
         <FontAwesomeIcon id="burger" icon={faBars} />
         <ul id="menuContent">
           <li class="menuItem" id="link1">
-            <a class="menuLink" href="/">about</a>
+            <Link class="menuLink" to='/'>about</Link>
+            {/* <a class="menuLink" href="/">about</a> */}
           </li>
           <li class="menuItem" id="link2">
-            <a class="menuLink" href="/digital">apps</a>
+            <Link class="menuLink" to='/digital'>apps</Link>
+            {/* <a class="menuLink" href="/digital">apps</a> */}
           </li>
           <li class="menuItem" id="link3">
-            <a class="menuLink" href="/handmade">handmade</a>
+            <Link class="menuLink" to='/handmade'>handmade</Link>
+            {/* <a class="menuLink" href="/handmade">handmade</a> */}
           </li>
         </ul>
       </section>
