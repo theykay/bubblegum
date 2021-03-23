6fead7e5f994b857b74a3e51046d14477d79b5cb
TheyKay <murraykyleb@gmail.com>
2021-03-23 16:55:08 -0600

    added notes for what props to pass

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './Project.scss';
 
-const Project = () => {
+// props:
+// class -> handProject || appProject
+// image -> import in parent
+// info -> blurb about project
+// site -> link to live app (only relevant for apps)
+// repo -> link to repository (only relevant for apps)
+// medium -> array containing languages or materials used
+
+const Project = (props) => {
   return (
     <>
+      <section className={props.class + ' projectCard'}>
+
+      </section>
     </>
   )
 };
