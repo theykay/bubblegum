82d9148233d9a29a95997e75e401d5daa3d6aad2
TheyKay <murraykyleb@gmail.com>
2021-03-22 21:24:43 -0600

    added other fontawesome packages, added icons to aboutme page

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './AboutMe.scss';
-// import Header from '../../Components/Header';
-// import Footer from '../../Components/Footer';
-import ProfilePic from '../../images/profile.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare, faLinkedin, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import ProfilePic from '../../images/profile.jpeg';
 
 const AboutMe = () => {
   return (
@@ -14,7 +15,10 @@ const AboutMe = () => {
 
         </section>
         <section id="meLinks">
-
+          <Link id="githubLink" className="profLink" to="https://github.com/theykay"><FontAwesomeIcon icon={faGithubSquare} />theykay</Link>
+          <Link id="emailLink" className="profLink" to="mailto:murraykyleb@gmail.com"><FontAwesomeIcon icon={faEnvelopeSquare} />murraykyleb@gmail.com</Link>
+          <Link id="linkedLink" className="profLink" to="https://www.linked.com/in/theykay"><FontAwesomeIcon icon={faLinkedin} />theykay</Link>
+          <Link id="discordLink" className="profLink" to="https://discord.gg/sKc5mWq"><FontAwesomeIcon icon={faDiscord} />theykay</Link>
         </section>
       </article>
     </>
