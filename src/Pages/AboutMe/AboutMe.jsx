40e2c449a6a793a3520ea3957d95e7b4c7ea9ae2
TheyKay <murraykyleb@gmail.com>
2021-03-23 16:31:31 -0600

    styling aboutme page

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -10,20 +10,28 @@ import Resume from '../../images/KayMurray.pdf';
 const AboutMe = () => {
   return (
     <>
-      <article id="aboutMe">
-        <img id="profPic" src={ProfilePic} alt="me artistically draped around an overturned chair" />
-        <section id="metails">
-          <h3><FontAwesomeIcon icon={faMapMarkerAlt} />Utah</h3>
-          <p>Background as a textile goods maker and designer, education in industrial and web design, interested in accessibility design.</p>
-          <Link id="resumeLink" className="profLink" to={Resume} title="opens in new tab" target="_blank">resume</Link>
+      <section id="aboutMe">
+        <section>
+          <img id="profPic" src={ProfilePic} alt="me artistically draped around an overturned chair" />
+          <h3 id="location"><FontAwesomeIcon icon={faMapMarkerAlt} /> Utah</h3>
         </section>
-        <section id="meLinks">
-          <Link id="githubLink" className="profLink" to="https://github.com/theykay"><FontAwesomeIcon icon={faGithubSquare} />theykay</Link>
-          <Link id="emailLink" className="profLink" to="mailto:murraykyleb@gmail.com"><FontAwesomeIcon icon={faEnvelopeSquare} />murraykyleb@gmail.com</Link>
-          <Link id="linkedLink" className="profLink" to="https://www.linked.com/in/theykay"><FontAwesomeIcon icon={faLinkedin} />theykay</Link>
-          <Link id="discordLink" className="profLink" to="https://discord.gg/sKc5mWq"><FontAwesomeIcon icon={faDiscord} />theykay</Link>
+        <section>
+          <article id="metails">
+            <p>My background is in soft goods (making and designing), education in industrial and web design, and I'm interested in accessibility design.</p>
+            <Link id="resumeLink" className="profLink" to={Resume} title="opens in new tab" target="_blank">resume</Link>
+          </article>
+          <section id="meLinks">
+            <h3>Find me at <span id="username">theykay</span> on</h3>
+            <Link id="githubLink" className="profLink" to="https://github.com/theykay" title=""><FontAwesomeIcon icon={faGithubSquare} /></Link>
+            <Link id="linkedLink" className="profLink" to="https://www.linked.com/in/theykay"><FontAwesomeIcon icon={faLinkedin} /></Link>
+            <Link id="discordLink" className="profLink" to="https://discord.gg/sKc5mWq"><FontAwesomeIcon icon={faDiscord} /></Link>
+            <Link id="emailLink" className="profLink" to="mailto:murraykyleb@gmail.com"><FontAwesomeIcon icon={faEnvelopeSquare} /> murraykyleb@gmail.com</Link>
+          </section>
         </section>
-      </article>
+        <article id="plsContinue">
+          {/* random stuff about me */}
+        </article>
+      </section>
     </>
   )
 };
