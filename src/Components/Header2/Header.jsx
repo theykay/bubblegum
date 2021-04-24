d6092ec4bfda1e2cd8ccaddb7e8a58147c67c946
TheyKay <murraykyleb@gmail.com>
2021-04-24 15:32:07 -0600

    started header2 element for one page smooth scroll

--- src/Components/Header2/Header.jsx
+++ src/Components/Header2/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 const Header = () => {
   return (
-    <>
-    </>
+    <nav id="header" className="navbar">
+      <ul className="h-flex">
+        <li>
+          <Link activeClass="active" to="home" spy={true} smooth={true}>Home</Link>
+        </li>
+        <li>
+          <Link to="about" spy={true} smooth={true}>About</Link>
+        </li>
+        <li>
+          <Link to="skills" spy={true} smooth={true}>Skills</Link>
+        </li>
+        <li>
+          <Link to="projects" spy={true} smooth={true}>Projects</Link>
+        </li>
+        <li>
+          <Link to="links" spy={true} smooth={true}>Links</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
