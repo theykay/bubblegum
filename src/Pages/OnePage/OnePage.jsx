9f92b2ac629757327b9d8bb83553c904e2715f7b
TheyKay <murraykyleb@gmail.com>
2021-04-26 20:53:51 -0600

    trying to add svg background

--- src/Pages/OnePage/OnePage.jsx
+++ src/Pages/OnePage/OnePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './OnePage.scss';
 import Navbar from '../../Components/Navbar';
+import Clouds from '../../Components/Clouds';
 
 const OnePage = () => {
   return (
     <>
       <Navbar />
+      <Clouds />
     </>
   )
 };
