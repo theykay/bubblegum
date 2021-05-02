cb932c5595f05ce5674116e4aef1990dfd2f53b2
TheyKay <murraykyleb@gmail.com>
2021-05-02 17:54:38 -0600

    about me section filled out and styled for mobile

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -5,8 +5,12 @@ import myPic from '../../images/profile.jpeg';
 const About = () => {
   return (
     <section id="about" className="glass-card">
-      <img id="profilePic" src={myPic} alt="me artfully draped around an overturned chair"/>
       <header className="glass-card-head">about me</header>
+      <img id="profilePic" src={myPic} alt="me artfully draped around an overturned chair" />
+      <article className="glass-card-content">
+        <p>I'm Kay, a web developer working to make accessible applications. I have a BFA in Industrial Design, and a Full Stack Web Development certificate. I'm excited about continuing to learn and implement principles of accessibility and universal design to build applications with a more diverse range of users in mind.</p>
+        <p>Mainly I'm interested in making things, using anything from code to textiles.</p>
+      </article>
     </section>
   )
 }
