f4943b3c805504708049f003f6e0632317359759
TheyKay <murraykyleb@gmail.com>
2021-05-02 20:17:44 -0600

    added content to contact and skills sections

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -5,7 +5,7 @@ import myPic from '../../images/profile.jpeg';
 const About = () => {
   return (
     <section id="about" className="glass-card">
-      <header className="glass-card-head">about me</header>
+      <header className="glass-card-head"><h2>about me</h2></header>
       <img id="profilePic" src={myPic} alt="me artfully draped around an overturned chair" />
       <article className="glass-card-content">
         <p>I'm Kay, a web developer working to make accessible applications. I have a BFA in Industrial Design, and a Full Stack Web Development certificate. I'm excited about continuing to learn and implement principles of accessibility and universal design to build applications with a more diverse range of users in mind.</p>
--- src/Sections/Skills/Skills.jsx
+++ src/Sections/Skills/Skills.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import './Skills.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faHtml5, faCss3Alt, faSass, faJsSquare, faReact, faBootstrap, faNodeJs, faNpm, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Skills = () => {
   return (
     <section id="skills" className="glass-card">
-
+      <header className="glass-card-head"><h2>skills</h2></header>
+      <article className="glass-card-content">
+      <ul id="technologies">
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faHtml5} />
+            <p>HTML5</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faCss3Alt} />
+            <p>CSS3</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faSass} />
+            <p>SCSS</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faJsSquare} />
+            <p>JavaScript/jQuery</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faNodeJs} />
+            <p>Node.js/Express</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faNpm} />
+            <p>npm.js</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faReact} />
+            <p>React.js</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faBootstrap} />
+            <p>Bootstrap</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faGithub} />
+            <p>Git/GitHub</p>
+          </li>
+          <li className="techItem">
+            <FontAwesomeIcon className="techIcon" icon={faDatabase} />
+            <p>SQL,MongoDB</p>
+          </li>
+        </ul>
+      </article>
     </section>
   )
 }
