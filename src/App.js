d66f1a330198eb811a304044b882d5a73d88a742
TheyKay <murraykyleb@gmail.com>
2021-05-02 18:56:56 -0600

    working on scrolling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import AboutMe from "./Pages/AboutMe";
-import Handmade from "./Pages/Handmade";
-import WebDesign from "./Pages/WebDesign";
+// import Header from './Components/Header';
+// import Footer from './Components/Footer';
+// import AboutMe from "./Pages/AboutMe";
+// import Handmade from "./Pages/Handmade";
+// import WebDesign from "./Pages/WebDesign";
 import OnePage from './Pages/OnePage';
 
 const App = () => {
