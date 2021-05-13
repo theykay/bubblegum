94f3e53a0445be82cd56df7708ffd1fa8e096b97
TheyKay <murraykyleb@gmail.com>
2021-05-13 11:44:05 -0600

    amade totop only visible once window has scrolled past a certain point

--- src/Components/ToTop/ToTop.jsx
+++ src/Components/ToTop/ToTop.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ToTop.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
 
 const ToTop = () => {
+  const [showMe, setShowMe] = useState(false);
+
+  const showHide = () => {
+    setShowMe(false)
+    if (window.scrollY > 200) {
+      setShowMe(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', showHide)
+  }, [])
+
   return (
-    <div id="to-top">
-      <a href="#about">
-        <FontAwesomeIcon id="to-top-icon" icon={faAngleDoubleUp} />
-      </a>
-    </div>
+    <>
+      { showMe &&
+        <div id="to-top">
+          <a href="#about">
+            <FontAwesomeIcon id="to-top-icon" icon={faAngleDoubleUp} />
+          </a>
+        </div>
+      }
+    </>
   )
 }
 
