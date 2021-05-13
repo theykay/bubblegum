220b3e7991e45e64c4f16bbe43dd2a299039f1f5
TheyKay <murraykyleb@gmail.com>
2021-05-12 19:19:48 -0600

    created totop component and moved styling from onepage to component

--- src/Components/ToTop/ToTop.jsx
+++ src/Components/ToTop/ToTop.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './ToTop.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
 
 const ToTop = () => {
   return (
-    <>
-    </>
+    <div id="to-top">
+      <a href="#about">
+        <FontAwesomeIcon id="to-top-icon" icon={faAngleDoubleUp} />
+      </a>
+    </div>
   )
 }
 
